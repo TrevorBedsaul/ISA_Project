a3d7495f24feeafb0ed100133e16ba0dcfb2563c
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-02-16 13:46:35 -0500

    Add models to admin portal

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book, Buyer, Seller, GenericUser
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Buyer)
+admin.site.register(Seller)
+admin.site.register(GenericUser)
