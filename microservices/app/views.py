6d2b5d49bc9c02f0a8123f3186ef88b3bebf28da
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-03-01 10:41:22 -0500

    Make microservice error handling more consistent

--- microservices/app/views.py
+++ microservices/app/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render
 from .models import Book, Buyer, Seller, GenericUser
 from django.core.exceptions import ObjectDoesNotExist
 from django.forms.models import model_to_dict
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse
 import json
 
 # Create your views here.
 
 def get_book(request, book_id):
     if request.method != "GET":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use GET instead)"}), status=405)
     try:
         book_object = Book.objects.get(id=book_id)
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Book not found"}))
+        return HttpResponse(json.dumps({"error":"Book not found"}), status=404)
     return HttpResponse(json.dumps(model_to_dict(book_object)), status=200)
 
 def update_book(request, book_id):
     if request.method != "POST":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use POST instead)"}), status=405)
     try:
         book_object = Book.objects.get(id=book_id)
         for key, value in request.POST.items():
@@ -26,81 +26,82 @@ def update_book(request, book_id):
         book_object.save()
         book_object = Book.objects.get(id=book_id)
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Book not found"}))
-    except:
-        return HttpResponseNotFound(json.dumps({"Error":"Saving book failed"}))
+        return HttpResponse(json.dumps({"error": "Book not found"}), status=404)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     return HttpResponse(json.dumps(model_to_dict(book_object)), status=200)
 
 
 def create_book(request):
     if request.method != "POST":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use POST instead)"}), status=405)
+
     try:
-        title = request.POST.get("title")
-        ISBN = request.POST.get("ISBN")
-        author = request.POST.get("author")
-        price = request.POST.get("price")
-        year = request.POST.get("year")
-        class_id = request.POST.get("class_id")
-        edition = request.POST.get("edition")
-        type_name = request.POST.get("type")
-        condition = request.POST.get("condition")
-        seller_id = request.POST.get("seller")
-    except:
-        return HttpResponse(status=400)
+        title = request.POST["title"]
+        ISBN = request.POST["ISBN"]
+        author = request.POST["author"]
+        price = request.POST["price"]
+        year = request.POST["year"]
+        class_id = request.POST["class_id"]
+        edition = request.POST["edition"]
+        type_name = request.POST["type"]
+        condition = request.POST["condition"]
+        seller_id = request.POST["seller"]
+    except KeyError as e:
+        return HttpResponse(json.dumps({"error": "Key not found: " + e.args[0]}), status=400)
 
     try:
         seller_object = Seller.objects.get(id=seller_id)
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Seller not found"}))
+        return HttpResponse(json.dumps({"error": "Seller not found"}), status=404)
 
     try:
         book_object = Book(title=title, ISBN=ISBN, author=author, price=price, year=year, class_id=class_id,
                            edition=edition, type_name=type_name, condition=condition, seller=seller_object)
         book_object.save()
-    except:
-        return HttpResponse(status=500)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     return HttpResponse(json.dumps(model_to_dict(book_object)), status=200)
 
 
 def delete_book(request, book_id):
     if request.method != "GET":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use GET instead)"}), status=405)
     try:
         book_object = Book.objects.get(id=book_id)
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Book not found"}))
+        return HttpResponse(json.dumps({"error":"Book not found"}), status=404)
     try:
         book_object.delete()
-    except:
-        HttpResponse(status=500)
+    except Exception as e:
+        HttpResponse(json.dumps({"error": str(type(e))}), status=500)
     return HttpResponse(json.dumps(model_to_dict(book_object)), status=200)
 
 
 def delete_seller(request, seller_id):
     if request.method != "GET":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use GET instead)"}), status=405)
     try:
         seller_object = Seller.objects.get(id=seller_id)
         generic_user = seller_object.generic_user
         seller_object.generic_user = generic_user
         seller_object.delete()
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Seller not found"}))
-    except:
-        return HttpResponseNotFound(json.dumps({"Error":"Deleting Seller failed"}))
+        return HttpResponse(json.dumps({"error": "Seller not found"}), status=404)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
     return HttpResponse(json.dumps(model_to_dict(seller_object)), status=200)
 
 def get_seller(request, seller_id):
     if request.method != "GET":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use GET instead)"}), status=405)
     try:
         seller_object = Seller.objects.get(id=seller_id)
         generic_user = seller_object.generic_user
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Seller not found"}))
+        return HttpResponse(json.dumps({"error":"Seller not found"}), status=404)
     generic_user_dict = model_to_dict(generic_user)
     del generic_user_dict["password"]
     seller_dict = model_to_dict(seller_object)
@@ -110,10 +111,9 @@ def get_seller(request, seller_id):
 
 def update_seller(request, seller_id):
     if request.method != "POST":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use POST instead)"}), status=405)
     try:
         seller_object = Seller.objects.get(id=seller_id)
-        generic_user = seller_object.generic_user
         generic_user_id = int(seller_object.generic_user.id)
         generic_user = GenericUser.objects.get(id=generic_user_id)
 
@@ -129,9 +129,9 @@ def update_seller(request, seller_id):
         seller_object.save()
         seller_object = Seller.objects.get(id=seller_id)
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Seller not found"}))
-    except:
-        return HttpResponseNotFound(json.dumps({"Error":"Saving Seller failed"}))
+        return HttpResponse(json.dumps({"error":"Seller not found"}), status=404)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     generic_user_dict = model_to_dict(generic_user)
     del generic_user_dict["password"]
@@ -144,10 +144,9 @@ def update_seller(request, seller_id):
 
 def update_buyer(request, buyer_id):
     if request.method != "POST":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use POST instead)"}), status=405)
     try:
         buyer_object = Buyer.objects.get(id=buyer_id)
-        generic_user = buyer_object.generic_user
         generic_user_id = int(buyer_object.generic_user.id)
         generic_user = GenericUser.objects.get(id=generic_user_id)
 
@@ -163,9 +162,9 @@ def update_buyer(request, buyer_id):
         buyer_object.save()
         buyer_object = Buyer.objects.get(id=buyer_id)
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Buyer not found"}))
-    except:
-        return HttpResponseNotFound(json.dumps({"Error":"Saving Buyer failed"}))
+        return HttpResponse(json.dumps({"error":"Buyer not found"}), status=404)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     generic_user_dict = model_to_dict(generic_user)
     del generic_user_dict["password"]
@@ -177,12 +176,12 @@ def update_buyer(request, buyer_id):
 
 def get_buyer(request, buyer_id):
     if request.method != "GET":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use GET instead)"}), status=405)
     try:
         buyer_object = Buyer.objects.get(id=buyer_id)
         generic_user = buyer_object.generic_user
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Buyer not found"}))
+        return HttpResponse(json.dumps({"Error":"Buyer not found"}), status=404)
     generic_user_dict = model_to_dict(generic_user)
     del generic_user_dict["password"]
     seller_dict = model_to_dict(buyer_object)
@@ -192,36 +191,38 @@ def get_buyer(request, buyer_id):
 
 def create_seller(request):
     if request.method != "POST":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use POST instead)"}), status=405)
 
     try:
         if "generic_user" in request.POST.keys():
             generic_user_id = int(request.POST.get("generic_user"))
             generic_user = GenericUser.objects.get(id=generic_user_id)
         else:
-            name = request.POST.get("name")
-            phone = request.POST.get("phone")
-            email = request.POST.get("email")
-            password = request.POST.get("password")
-            username = request.POST.get("username")
+            name = request.POST["name"]
+            phone = request.POST["phone"]
+            email = request.POST["email"]
+            password = request.POST["password"]
+            username = request.POST["username"]
             generic_user = GenericUser(name=name, phone=phone, email=email, password=password, username=username)
             generic_user.save()
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"GenericUser not found"}))
-    except:
-        return HttpResponse(status=400)
+        return HttpResponse(json.dumps({"error":"GenericUser not found"}), status=404)
+    except KeyError as e:
+        return HttpResponse(json.dumps({"error": "Key not found: " + e.args[0]}), status=400)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     try:
         seller_object = Seller(generic_user=generic_user, rating=0, activity_score=0)
         seller_object.save()
-    except:
-        return HttpResponse(status=500)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     return HttpResponse(json.dumps(model_to_dict(seller_object)), status=201)
 
 def create_buyer(request):
     if request.method != "POST":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use POST instead)"}), status=405)
 
     try:
         address = request.POST["address"]
@@ -237,28 +238,30 @@ def create_buyer(request):
             generic_user = GenericUser(name=name, phone=phone, email=email, password=password, username=username)
             generic_user.save()
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"GenericUser not found"}))
-    except:
-        return HttpResponse(status=400)
+        return HttpResponse(json.dumps({"error":"GenericUser not found"}), status=404)
+    except KeyError as e:
+        return HttpResponse(json.dumps({"error": "Key not found: " + e.args[0]}), status=400)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     try:
         buyer_object = Buyer(generic_user=generic_user, rating=0, activity_score=0, address=address)
         buyer_object.save()
-    except:
-        return HttpResponse(status=500)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
     return HttpResponse(json.dumps(model_to_dict(buyer_object)), status=201)
 
 def delete_buyer(request, buyer_id):
     if request.method != "GET":
-        return HttpResponse(status=405)
+        return HttpResponse(json.dumps({"error":"incorrect method (use GET instead)"}), status=405)
     try:
         buyer_object = Buyer.objects.get(id=buyer_id)
         generic_user = buyer_object.generic_user
         buyer_object.generic_user = generic_user
         buyer_object.delete()
     except ObjectDoesNotExist:
-        return HttpResponseNotFound(json.dumps({"Error":"Buyer not found"}))
-    except:
-        return HttpResponseNotFound(json.dumps({"Error":"Deleting Buyer failed"}))
+        return HttpResponse(json.dumps({"error":"Buyer not found"}), status=404)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
     return HttpResponse(json.dumps(model_to_dict(buyer_object)), status=200)
