78ce7f434e6609366a3aa745afce8832f0c53591
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-03-09 17:22:26 -0500

    Display Buyer and Seller names for books correctly

--- experience/experience_app/views.py
+++ experience/experience_app/views.py
@@ -18,13 +18,32 @@ def home(request):
 
 
 def book_detail(request, book_id):
-    req = urllib.request.Request('http://models-api:8000/api/v1/books/' + str(book_id))
+    book_req = urllib.request.Request('http://models-api:8000/api/v1/books/' + str(book_id))
 
     try:
-        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        book_json = urllib.request.urlopen(book_req).read().decode('utf-8')
     except HTTPError as e:
         return HttpResponse(json.dumps({"error": e.msg}), status=e.code)
     except Exception as e:
         return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
 
-    return HttpResponse(resp_json, status=200)
+    book = json.loads(book_json)
+    seller_id = book['seller']
+    has_buyer = (book['buyer'] != None)
+    seller_req = urllib.request.Request('http://models-api:8000/api/v1/sellers/' + str(seller_id))
+    try:
+        seller_json = urllib.request.urlopen(seller_req).read().decode('utf-8')
+        if has_buyer:
+            buyer_req = urllib.request.Request('http://models-api:8000/api/v1/buyers/' + str(book['buyer']))
+            buyer_json = urllib.request.urlopen(buyer_req).read().decode('utf-8')
+    except HTTPError as e:
+        return HttpResponse(json.dumps({"error": e.msg}), status=e.code)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
+
+    seller = json.loads(seller_json)
+    book['seller'] = {'id':seller['id'], 'name':seller['generic_user']['name']}
+    if has_buyer:
+        buyer = json.loads(buyer_json)
+        book['buyer'] = {'id': buyer['id'], 'name': buyer['generic_user']['name']}
+    return HttpResponse(json.dumps(book), status=200)
