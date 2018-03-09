3df1fa7bbc60d1b8a1739d9f35725c5b9b647081
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-03-09 16:40:29 -0500

    Create trivial homepage for web layer

--- web_layer/web_layer/urls.py
+++ web_layer/web_layer/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from web_layer_app import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^home', views.home, name='homepage'),
 ]
