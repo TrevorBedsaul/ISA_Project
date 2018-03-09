1d577c026995f7e863f390455d4c180673f6de49
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-03-09 16:49:05 -0500

    Create homepage with titles of books

--- web_layer/web_layer_app/views.py
+++ web_layer/web_layer_app/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import urllib.request
+from urllib.error import HTTPError
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    req = urllib.request.Request('http://exp-api:8000/api/v1/home')
+    try:
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    except HTTPError as e:
+        return HttpResponse(json.dumps({"error": e.msg}), status=e.code)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": str(type(e))}), status=500)
+
+    book_list = json.loads(resp_json)
+    return render(request, "home.html", {"book_list":book_list})
