3ab7daf8c655d44551757ed8dcc7d0211e5e8c01
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-03-20 21:29:47 -0400

    Fix microservice tests to reflect SiteUser model

--- microservices/app/tests.py
+++ microservices/app/tests.py
@@ -2,57 +2,31 @@ from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
 
 
-class TestBuyers(TestCase):
+class TestUsers(TestCase):
 
     def setUp(self):
         pass
 
     def testBuyer(self):
         c = Client()
-        createResponse = c.post("/api/v1/buyers/create", {"name": "John Doe", "phone": "123456789", "email": "hello@gmail.com", "password": "pwd", "username": "JDoe", "address": "Earth", "rating": 9.0, "activity_score": 5.0})
-        self.assertAlmostEquals(createResponse.status_code, 201)
+        createResponse = c.post("/api/v1/users/create", {"name": "John Doe", "phone": "123456789", "email": "hello@gmail.com", "password": "pwd", "username": "JDoe", "address": "Earth", "rating": 9.0, "activity_score": 5.0})
+        self.assertEquals(createResponse.status_code, 201)
 
-        getResponseValid = c.get("/api/v1/buyers/1")
-        self.assertAlmostEquals(getResponseValid.status_code, 200)
+        getResponseValid = c.get("/api/v1/users/1")
+        self.assertEquals(getResponseValid.status_code, 200)
 
-        getResponseInValid = c.get("/api/v1/buyers/3")
-        self.assertAlmostEquals(getResponseInValid.status_code, 404)
-        updateResponse = c.post("/api/v1/buyers/1/update", {"email": "newemail@yahoo.com"})
-        self.assertAlmostEquals(updateResponse.status_code, 200)
-        deleteResponseValid = c.get("/api/v1/buyers/1/delete")
-        self.assertAlmostEquals(deleteResponseValid.status_code, 200)
-        deleteResponseInValid = c.post("/api/v1/buyers/3/delete")
-        self.assertAlmostEquals(deleteResponseInValid.status_code, 405)
-
-    def tearDown(self):
-        pass
-
-
-class TestSellers(TestCase):
-
-    def setUp(self):
-        pass
-
-    def testSeller(self):
-        c = Client()
-        createResponse = c.post("/api/v1/sellers/create", {"name": "John Doe", "phone": "123456789", "email": "hello@gmail.com", "password": "pwd", "username": "JDoe"})
-        self.assertAlmostEquals(createResponse.status_code, 201)
-
-        getResponseValid = c.get("/api/v1/sellers/2")
-        self.assertAlmostEquals(getResponseValid.status_code, 200)
-
-        getResponseInValid = c.get("/api/v1/sellers/3")
-        self.assertAlmostEquals(getResponseInValid.status_code, 404)
-
-        updateResponse = c.post("/api/v1/sellers/2/update", {"email": "newemail@yahoo.com"})
-        self.assertAlmostEquals(updateResponse.status_code, 200)
-
-        deleteResponseValid = c.get("/api/v1/sellers/2/delete")
-        self.assertAlmostEquals(deleteResponseValid.status_code, 200)
-
+        getResponseInValid = c.get("/api/v1/users/3")
+        self.assertEquals(getResponseInValid.status_code, 404)
+        updateResponse = c.post("/api/v1/users/1/update", {"email": "newemail@yahoo.com"})
+        self.assertEquals(updateResponse.status_code, 200)
+        deleteResponseValid = c.get("/api/v1/users/1/delete")
+        self.assertEquals(deleteResponseValid.status_code, 200)
+        deleteResponseInValid = c.post("/api/v1/users/3/delete")
+        self.assertEquals(deleteResponseInValid.status_code, 405)
 
     def tearDown(self):
         pass
+
 
 
 class TestBooks(TestCase):
@@ -68,17 +42,17 @@ class TestBooks(TestCase):
         createResponse = c.post("/api/v1/books/create",
                           {"title": "Intro to Politics", "ISBN": "343459789", "author": "David Wallace", "price": 95.0,
                            "year": "2014", "class_id": "POLI 1010", "edition":1,"type":"HC","condition":"NW","seller":"1"})
-        self.assertAlmostEquals(createResponse.status_code, 201)
+        self.assertEquals(createResponse.status_code, 201)
         getResponseValid = c.get("/api/v1/books/1")
-        self.assertAlmostEquals(getResponseValid.status_code, 200)
+        self.assertEquals(getResponseValid.status_code, 200)
         getResponseInValid = c.get("/api/v1/books/3")
-        self.assertAlmostEquals(getResponseInValid.status_code, 404)
+        self.assertEquals(getResponseInValid.status_code, 404)
         updateResponse = c.post("/api/v1/books/1/update", {"price": 85.0})
-        self.assertAlmostEquals(updateResponse.status_code, 200)
+        self.assertEquals(updateResponse.status_code, 200)
         deleteResponseInValid = c.post("/api/v1/books/1/delete")
-        self.assertAlmostEquals(deleteResponseInValid.status_code, 405)
+        self.assertEquals(deleteResponseInValid.status_code, 405)
         deleteResponseValid = c.get("/api/v1/books/1/delete")
-        self.assertAlmostEquals(deleteResponseValid.status_code, 200)
+        self.assertEquals(deleteResponseValid.status_code, 200)
 
 
 
