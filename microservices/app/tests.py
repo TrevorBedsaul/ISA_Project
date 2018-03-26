bd6e5ea5a442184bf451f36bafc0455ea8439a9e
A-Mangels <am8gv@virginia.edu>
2018-03-26 18:44:49 -0400

    login test cases

--- microservices/app/tests.py
+++ microservices/app/tests.py
@@ -15,8 +15,8 @@ class TestUsers(TestCase):
         getResponseValid = c.get("/api/v1/users/1")
         self.assertEquals(getResponseValid.status_code, 200)
 
-        getResponseInValid = c.get("/api/v1/users/3")
-        self.assertEquals(getResponseInValid.status_code, 404)
+        getResponseValid = c.get("/api/v1/users/?username=danny")
+        self.assertEquals(getResponseValid.status_code, 200)
         updateResponse = c.post("/api/v1/users/1/update", {"email": "newemail@yahoo.com"})
         self.assertEquals(updateResponse.status_code, 200)
         deleteResponseValid = c.get("/api/v1/users/1/delete")
@@ -36,16 +36,16 @@ class TestBooks(TestCase):
 
     def testBook(self):
         c = Client()
-        response = c.post("/api/v1/sellers/create",
+        response = c.post("/api/v1/users/create",
                           {"name": "Sarah Jane", "phone": "987654321", "email": "sjane@gmail.com", "password": "pass",
-                           "username": "SJane", "rating": 7.5, "activity_score": 8.0})
+                           "username": "SJane",})
         createResponse = c.post("/api/v1/books/create",
                           {"title": "Intro to Politics", "ISBN": "343459789", "author": "David Wallace", "price": 95.0,
                            "year": "2014", "class_id": "POLI 1010", "edition":1,"type":"HC","condition":"NW","seller":"1"})
         self.assertEquals(createResponse.status_code, 201)
         getResponseValid = c.get("/api/v1/books/1")
         self.assertEquals(getResponseValid.status_code, 200)
-        getResponseInValid = c.get("/api/v1/books/3")
+        getResponseInValid = c.get("/api/v1/books/?ISBN=1256897")
         self.assertEquals(getResponseInValid.status_code, 404)
         updateResponse = c.post("/api/v1/books/1/update", {"price": 85.0})
         self.assertEquals(updateResponse.status_code, 200)
@@ -55,6 +55,21 @@ class TestBooks(TestCase):
         self.assertEquals(deleteResponseValid.status_code, 200)
 
 
+
+    def tearDown(self):
+        pass
+
+class TestAuthentication(TestCase):
+
+    def setUp(self):
+        pass
+
+    def testAuthentication(self):
+        c = Client()
+
+        loginResponse = c.post("/api/v1/login", {"username": "danny", "password": "Password2"})
+
+        self.assertEquals(loginResponse.status_code, 200)
 
 
 
