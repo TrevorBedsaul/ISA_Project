fede91a17372c22217f7e79784a91d40fab2c377
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-04-02 17:49:46 -0400

    Create script to put messages in elasticSearch

--- batch/script.py
+++ batch/script.py
@@ -1,1 +1,17 @@
+from kafka import KafkaConsumer
+import json
+import time
+from elasticsearch import Elasticsearch
+
 print("hello from script.py")
+time.sleep(3)
+print("done sleeping")
+
+consumer = KafkaConsumer('new-listings-topic', group_id='listing-indexer', bootstrap_servers=['kafka:9092'])
+es = Elasticsearch(['es'])
+
+while True:
+    for message in consumer:
+        message_dict = json.loads((message.value).decode('utf-8'))
+        es.index(index='listing_index', doc_type='listing', id=message_dict['id'], body=message_dict)
+    es.indices.refresh(index="listing_index")
