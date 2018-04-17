7c72db1f807cc2b429b724b07b50dd3abca93de6
Danny Voce <davjr96@gmail.com>
2018-04-16 23:24:57 -0400

    Add mysql to docker-compose, no delay yet

--- setup-db.sh
+++
@@ -1,4 +0,0 @@
-docker pull mysql:5.7.14
-mkdir ~/cs4501/db
-docker run --name mysql -d -e MYSQL\_ROOT\_PASSWORD='$3cureUS' -v ~/cs4501/db:/var/lib/mysql  mysql:5.7.14
-docker run -it --name mysql-cmdline --link mysql:db mysql:5.7.14 bash
