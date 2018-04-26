821f3ea09dd1530748b0be37acaa371270c0145a
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-04-25 21:49:26 -0400

    Create login/logout test

--- selenium/selenium_tests.py
+++ selenium/selenium_tests.py
@@ -19,7 +19,6 @@ class TestTemplate(unittest.TestCase):
 
     def test_book_link(self):
         """Find and click book title button"""
-        self.assertEqual(1, 1)
         try:
             self.driver.get('http://web:8000/')
             el = self.driver.find_element_by_class_name('lead')
@@ -27,6 +26,27 @@ class TestTemplate(unittest.TestCase):
             info = self.driver.find_element_by_class_name("starter-template")
             contains_title = "Winnie the Pooh" in info.get_attribute("innerHTML")
             self.assertTrue(contains_title)
+        except NoSuchElementException as ex:
+            self.fail(ex.msg)
+
+    def test_login_logout(self):
+        """Log in using web interface"""
+        try:
+            self.driver.get('http://web:8000/login')
+            username = self.driver.find_element_by_id('id_username')
+            username.send_keys("stevendicarlo2")
+            password = self.driver.find_element_by_id('id_password')
+            password.send_keys("Password1")
+            submit = self.driver.find_element_by_class_name("btn-lg")
+            submit.click()
+            self.assertEqual(self.driver.title, "Homepage")
+
+            logout = self.driver.find_element_by_partial_link_text("Logout")
+            self.assertEqual(logout.get_attribute("href"), "http://web:8000/logout")
+            logout.click()
+
+            login_button = self.driver.find_element_by_partial_link_text("Login")
+            self.assertEqual(login_button.get_attribute("href"), "http://web:8000/login")
         except NoSuchElementException as ex:
             self.fail(ex.msg)
 
