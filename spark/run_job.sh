ab7ea87650549cafbba62d0ac14942eb42f978c5
Danny Voce <davjr96@gmail.com>
2018-05-07 22:03:03 -0400

    Fix spark script not running

--- spark/run_job.sh
+++ spark/run_job.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+bin/spark-class org.apache.spark.deploy.master.Master -h spark-master &
 while true
 do
     bin/spark-submit --master spark://spark-master:7077 --total-executor-cores 2 --executor-memory 512m /tmp/data/spark_job.py
